957e98a0414fa4549adf3b18cce287f88a3bb12d
cesar gutierrez <cesargusa22@gmail.com>
2020-12-09 13:37:39 +0100

    Añadidio metodo en le main para la LocalDate

--- src/main/java/com/example/NyW/ShopPage/NyWShopPageApplication.java
+++ src/main/java/com/example/NyW/ShopPage/NyWShopPageApplication.java
@@ -1,5 +1,8 @@
 package com.example.NyW.ShopPage;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,15 @@ public class NyWShopPageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NyWShopPageApplication.class, args);
+		
+		
 	}
+
+
+public static LocalDate da(){
+	LocalDate date = LocalDate.now();
+	return date;
+}
+
 
 }
