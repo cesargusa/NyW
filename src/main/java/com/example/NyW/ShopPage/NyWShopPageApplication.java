af78292e6da26bff03855939a363fa7c12b8035d
cesar gutierrez <cesargusa22@gmail.com>
2020-12-09 12:33:34 +0100

    Proyecto Inicial

---
+++ src/main/java/com/example/NyW/ShopPage/NyWShopPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.NyW.ShopPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyWShopPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyWShopPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NyW/ShopPage/NyWShopPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NyW.ShopPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyWShopPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
